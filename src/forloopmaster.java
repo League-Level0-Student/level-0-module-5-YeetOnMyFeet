f79e6194340e3c038404c002834b5027265ec3cb
seema <seema@DESKTOP-9RUA99R>
2020-04-23 18:18:37 -0700

    worked on module 5 level 0

---
+++ src/forloopmaster.java
@@ -0,0 +1,16 @@
+
+public class forloopmaster {
+public static void main(String[] args) {
+	for (int i = 1; i < 3; i++) {
+		
+		for (int j = 2; j < 3; j++) {
+			
+			for (int j2 = 3; j2 < 3; j2++) {
+				System.out.println(i + " " + j + " "+ j2);
+			}
+		}
+	}
+	
+}
+}
+
