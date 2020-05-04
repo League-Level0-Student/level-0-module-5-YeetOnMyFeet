e2ded2770b0a9c23ff8da89e60266ca15efc5abb
seema <seema@DESKTOP-9RUA99R>
2020-05-04 15:27:08 -0700

    almost done with level 0

--- src/_04_practice/_2_code_4_life/Code4Life.java
+++ src/_04_practice/_2_code_4_life/Code4Life.java
@@ -6,20 +6,28 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class Code4Life {
 
 	// 1. Ask the user how many hours they spent coding this week.
-
+String code = JOptionPane.showInputDialog("How many hours did u spend coding this week?");
+int name = Integer.parseInt(code);{
 	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
 	// write code instead.
-
+	if(name <= 2) {
+		JOptionPane.showMessageDialog(null, "Get off of YT and start coding!");
+	}
 	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
 	// they're a Code Ninja
-
+	if(name >= 3) {
+		JOptionPane.showMessageDialog(null, "You are a code ninja!");
+	}
 	// 4. If it is more than 5, call the method below to play the Batman theme song.
 	// NOTE: You may have to get batman.wav from league-sounds on github
-
+if(name > 5) {
+	playBatmanTheme();}
+}
 	private static void playBatmanTheme() {
 		try {
 			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
--- src/skillpractice.java
+++ src/skillpractice.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class skillpractice {
@@ -20,7 +22,7 @@ int cents = Integer.parseInt(dimes);
 
 
 		// Tell them how many cents they have (hint multiply by 10)
-JOptionPane.showMessageDialog(null, "You have " + cents*10 + "!" );
+JOptionPane.showMessageDialog(null, "You have " + cents*10 + "cents!" );
 
 
 
@@ -33,14 +35,18 @@ int tall = Integer.parseInt(height);
 if (tall < 36) {
 	JOptionPane.showMessageDialog(null, "Go eat your Wheaties!");
 }
-
+else if (tall >= 36){
+	JOptionPane.showMessageDialog(null, "You are tall!");
+}
 
 
 		 }
 
 		 void skill2() {// Write a loop to print every third number between 1 and 30 to the console 
 
-
+for (int i = 0; i < 30; i++) {
+	System.out.println(i);
+}
 
 
 
@@ -49,51 +55,74 @@ if (tall < 36) {
 		 }
 
 		 void skill3() {// Get a random number that is less than 20 and print it to the console 
+int num = 0;
+Random ran = new Random();
+num = ran.nextInt(20);
+if (num < 20) {
+System.out.println(num);
+}
 
 
 
 		// Get another random number that is less than 10 and print it to the console 
 
-
+if (num < 10) {
+	System.out.println(num);
+}
 
 		// Using a pop-up, tell the user the difference between the numbers// Hint: use subtraction 
-
+JOptionPane.showMessageDialog(null, num - num);
 
 
 			}
 
 		 void skill4() {// In a pop-up, ask the user for the city they live in 
-
+String city = JOptionPane.showInputDialog("What city do you live in?");
 
 
 		// If they answered "San Diego", tell them they live in America's Finest City 
-
+if (city == "San Diego") {
+	JOptionPane.showMessageDialog(null, "You live in America's Finest City!");
+}
 
 
 		// Otherwise, tell them to move to San Diego 
-
+else if (city != "San Diego") {
+	JOptionPane.showMessageDialog(null, "You should move to San Diego!");
+}
 
 
 		// Create a variable - cars - and initialize it to the number of cars your family has.// If there are 0 cars, use a pop-up to display, "I bet you use public transportation." 
-
+int cars = 3;
+if (cars == 0) {
+	JOptionPane.showMessageDialog(null, "I bet you use public transportation!");
+}
 
 
 		// If there is 1 car, use a pop-up to display the make/model of the car 
 
-
+else if (cars == 1) {
+	String model = JOptionPane.showInputDialog("What is your make/model of the car");
+	JOptionPane.showMessageDialog(null, model);
+}
 
 		// If there is more than 1 car, use a pop-up to display how many wheels the// cars have between them. 
-
+else if (cars > 1) {
+	String tires = JOptionPane.showInputDialog("How many cars do you have?");
+	int tire = Integer.parseInt(tires);
+	JOptionPane.showMessageDialog(null, "You have " + tire*4 + "tires!");
+}
 
 
 		 }
 
 		 void skill5() {// In a pop-up, ask the user for the name of their school 
-
+String school = JOptionPane.showInputDialog("What is the name of your school?");
+int name = Integer.parseInt(school);
 
 
 		// In another pop-up, tell the user, that their school is a fantastic school.// You must include the name of the school in the message. 
-
+JOptionPane.showMessageDialog(null, name + " is the best school!");
 
 
 		 }
